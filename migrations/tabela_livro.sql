b3a90aed86e1803729045e5a126f744e7db6470d
danielcb2021 <94814745+danielcb2021@users.noreply.github.com>
2024-01-05 20:13:43 -0300

    Delete migrations directory

--- migrations/tabela_livro.sql
+++
@@ -1,7 +0,0 @@
-create table livro (
- 	id int PRIMARY KEY AUTO_INCREMENT,
-    titulo varchar(300) not null,
-    autor varchar(300),
-    data_lancamento date,
-    genero varchar(30)
-);
--- migrations/tabela_usuario.sql
+++
@@ -1,8 +0,0 @@
-create table usuario (
- 	id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(300) not null,
-    data_nascimento varchar(10),
-    genero varchar(20),
-    email varchar(70),
-    senha varchar(50)
-);
